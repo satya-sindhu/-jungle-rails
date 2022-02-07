015e488b231712de40cd26e9124079091efaea5c
satya sindhu <movva001@gmail.com>
2022-02-07 17:44:40 -0500

    add product spec file and write initial test to ensure products are saved properly

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    describe 'Validations' do
+        it 'saves properly when all fields are filled in correctly' do
+          @category = Category.new(name: 'Miscellaneous')
+          @product = Product.new(name: 'Basketball', price: 3000, quantity: 48, category: @category)
+    
+          expect(@product).to be_valid
+        end
+      end
 end
