458e90e50020acb9784e7c1e361a58dba186d07d
satya sindhu <movva001@gmail.com>
2022-02-07 18:04:40 -0500

    Add test case for password and confirmation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,5 +2,16 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
     describe 'Validations' do
+        it 'should not save when the password field is left blank' do
+            @user = User.new(first_name: 'Yuti', last_name: 'Reswick', email: 'yuti@test.com', password: nil, password_confirmation: 'password')
+      
+            expect(@user).not_to be_valid
+          end
+      
+          it 'should not save when the password confirmation field is left blank' do
+            @user = User.new(first_name: 'Yuti', last_name: 'Reswick', email: 'yuti@test.com', password: 'password', password_confirmation: nil)
+      
+            expect(@user).not_to be_valid
+          end
     end
 end
