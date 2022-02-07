0f8df30b0f5100d1312220e158fd9a0caf9af5b2
satya sindhu <movva001@gmail.com>
2022-02-07 18:07:14 -0500

    Add test case for first_name and last_name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,13 +3,25 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
     describe 'Validations' do
         it 'should not save when the password field is left blank' do
-            @user = User.new(first_name: 'Yuti', last_name: 'Reswick', email: 'yuti@test.com', password: nil, password_confirmation: 'password')
+            @user = User.new(first_name: 'Darren', last_name: 'Brown', email: 'Darren@test.com', password: nil, password_confirmation: 'password')
       
             expect(@user).not_to be_valid
           end
       
           it 'should not save when the password confirmation field is left blank' do
-            @user = User.new(first_name: 'Yuti', last_name: 'Reswick', email: 'yuti@test.com', password: 'password', password_confirmation: nil)
+            @user = User.new(first_name: 'Darren', last_name: 'Brown', email: 'Darren@test.com', password: 'password', password_confirmation: nil)
+      
+            expect(@user).not_to be_valid
+          end
+
+          it 'should not save when the first name field is left blank' do
+            @user = User.new(first_name: nil, last_name: 'Brown', email: 'Darren@test.com', password: 'password', password_confirmation: 'password')
+      
+            expect(@user).not_to be_valid
+          end
+      
+          it 'should not save when the last name field is left blank' do
+            @user = User.new(first_name: 'Darren', last_name: nil, email: 'Darren@test.com', password: 'password', password_confirmation: 'password')
       
             expect(@user).not_to be_valid
           end
